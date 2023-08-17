3e00f0d757afa73ffd434ae51deeabc70601bde2
ariafatah <ariafatah0711@gmail.com>
2023-08-17 23:15:55 +0700

    pp

---
+++ js 1/05 - operator/script.js
@@ -0,0 +1,23 @@
+// ######################################################################################################
+// operasi aritmatika
+var x = 10;
+var y = 5;
+var hasilPenjumlahan = x + y; // result 15
+var hasilPengurangan = x - y; // result 5
+var hasilPerkalian = x * y; // result 50
+var hasilPembagian = x / y; // result 2
+var sisaHasilBagi = x % y; // result 0
+var hasilPangkat = x ** y; // result 100000
+
+console.log(
+  hasilPenjumlahan +
+    hasilPengurangan +
+    hasilPerkalian +
+    hasilPembagian +
+    sisaHasilBagi +
+    hasilPangkat
+);
+
+// operasi perbandingan
+var a = 10;
+var b = 5;
