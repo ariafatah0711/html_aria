4bf7c4e84f76f859dde01d3e549a6feb32cb8293
ariafatah <ariafatah0711@gmail.com>
2023-08-20 14:47:44 +0700

    p

---
+++ js 1/07 - perulangan/script.js
@@ -0,0 +1,21 @@
+// ######################################################################################################
+// perulangan counted
+
+// for
+for (let i = 0; i < 10; i++) {
+  console.log("perulangan ke-" + i);
+}
+// cara kerjanya adalah for lalu ada let i = 0 yang artinya membuat var i = 0
+// lalu ; i < 10 ini adalah kondisi jika i kurang dari 10 maka akan menjalankan,
+// ; i++ yang artinya variabel i ditambah 1 setiap diulang sampai kondisi menjadi i < 10 yaitu saat var i menjadi 10
+
+for (let i = 10; i > 0; i--) {
+  console.log("perulangan ke-" + i);
+}
+
+// perulangan while
+var i = 0;
+while (i < 10) {
+  console.log("perulagan ke-" + i);
+  i++;
+}
