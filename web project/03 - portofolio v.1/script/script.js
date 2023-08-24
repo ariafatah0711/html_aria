1471f346a1df6e438d54e750989db2e70c2a1f07
ariafatah <ariafatah0711@gmail.com>
2023-08-24 13:17:49 +0700

    p

--- web project/03 - portofolio v.1/script/script.js
+++ web project/03 - portofolio v.1/script/script.js
@@ -1,3 +1,27 @@
+// #######################################################################################################
+// anchor tag reset
+if (window.location.hash) {
+  //   window.locatioon.href = window.location.href.split("#")[0];
+  history.replaceState(null, null, window.location.href.split("#")[0]);
+}
+
+// header static to fixed
+const staticDiv = document.querySelector(".static-div");
+const section2 = document.querySelector("#section-2");
+
+function handleScroll() {
+  if (section2.getBoundingClientRect().top <= 0) {
+    staticDiv.classList.remove("static-div");
+    staticDiv.classList.add("fixed-div");
+  } else {
+    staticDiv.classList.remove("fixed-div");
+    staticDiv.classList.add("static-div");
+  }
+
+  window.addEventListener("scroll", handleScroll);
+}
+
+// smooth scroll
 const links = document.querySelectorAll("a");
 
 links.forEach((link) => {
