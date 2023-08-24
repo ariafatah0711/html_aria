7098c69bbf3b18e6b1fc51feb5f6522501d2aa6f
ariafatah <ariafatah0711@gmail.com>
2023-08-24 17:41:42 +0700

    p

--- web project/03 - portofolio v.1/script/script.js
+++ web project/03 - portofolio v.1/script/script.js
@@ -1,54 +1,15 @@
 // #######################################################################################################
 // anchor tag reset
 if (window.location.hash) {
-  //   window.locatioon.href = window.location.href.split("#")[0];
   history.replaceState(null, null, window.location.href.split("#")[0]);
 }
 
-const sections = document.querySelectorAll("section");
-const headers = document.querySelectorAll(".header");
-
-function setActiveHeader(index) {
-  headers.forEach((header, i) => {
-    if (i === index) {
-      header.style.display = "flex";
-    } else {
-      header.style.display = "none";
-    }
-  });
-}
-
-document.addEventListener("scroll", () => {
-  let activeSectionIndex = 0;
-  sections.forEach((section, index) => {
-    if (section.getBoundingClientRect().top <= 0) {
-      activeSectionIndex = index;
-    }
-  });
-  setActiveHeader(activeSectionIndex);
+// scroll link anchor
+document.getElementById("link1").addEventListener("click", function (event) {
+  event.preventDefault(); // mencegah aksi bawahan link
+  window.scrollBy(0, window.innerHeight * 2);
+  window.location.href = this.getAttribute("href"); //pergi ke tautan setelah scroll
 });
-
-// header static to fixed
-// const headers = document.querySelectorAll(".header");
-
-// function adjustZIndex(index) {
-//   headers.forEach((header, i) => {
-//     if (i === index) {
-//       header.style.zIndex = "10";
-//     } else {
-//       header.style.zIndex = "1";
-//     }
-//   });
-// }
-
-// document.addEventListener("scroll", () => {
-//   headers.forEach((header, index) => {
-//     const section = document.querySelector("#section-${index + 1}");
-//     if (section.getBoundingClientRect().top <= 0) {
-//       adjustZIndex(index);
-//     }
-//   });
-// });
 
 // smooth scroll
 const links = document.querySelectorAll("a");
