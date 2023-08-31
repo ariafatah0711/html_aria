a558bf1983e02fe1d54b5edee66946954c52a995
ariafatah <ariafatah0711@gmail.com>
2023-08-31 23:41:09 +0700

    update

---
+++ web project/03 - navbar responsive/script/script.js
@@ -0,0 +1,11 @@
+// onload check = false
+window.onload = function () {
+  const checkBox = document.getElementById("check");
+  checkBox.checked = false;
+};
+
+// click a[link] check = false
+function uncheckChecked() {
+  const checkBox = document.getElementById("check");
+  checkBox.checked = false;
+}
