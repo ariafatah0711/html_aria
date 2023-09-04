1d862087b5f0792721d37e0fea996baad9040b4a
ariafatah <ariafatah0711@gmail.com>
2023-09-04 10:37:04 +0700

    update

--- js 1/11 - DOM/script.js
+++ js 1/11 - DOM/script.js
@@ -15,8 +15,8 @@ document.title.length;
 // ########################################################################################################
 // mengakses elemen spesifik
 var div1 = document.getElementById("id");
-var div2 = document.getElementsByClassName("class");
-var button = document.getElementsByName("name");
+var div2 = document.getElementsByClassName("class")[0];
+var button = document.getElementsByName("name")[0];
 var link = document.getElementsByTagName("a");
 var pFirst = document.querySelector("p");
 var Pall = document.querySelectorAll("p");
@@ -28,4 +28,59 @@ div1.innerText =
 // memberikan CSS ke elemen
 div1.style.backgroundColor = "red";
 div2.style.backgroundColor = "blue";
-button.style.border = "1px solid green";
+button.style.border = "10px solid green";
+
+for (let i = 0; i < link.length; i++) {
+  link[i].style.textDecoration = "none";
+}
+
+pFirst.style.backgroundColor = "blue";
+for (let i = 0; i < Pall.length; i++) {
+  Pall[i].style.color = "red";
+}
+
+// ########################################################################################################
+// elemen dengan class, name, tag, dan queryselectorAll memiliki array
+var paragraf = document.getElementsByClassName("paragraf");
+console.log(paragraf); //=> array
+
+// membuat animasi warna menggunakan ser interval dan timeout
+setInterval(function () {
+  // menjalankan sesuatu secara berulang
+  paragraf[0].style.color = "red";
+  paragraf[1].style.color = "yellow";
+  paragraf[2].style.color = "green";
+
+  setTimeout(function () {
+    // menjalankan sesuatu setelah penundaan selesai
+    paragraf[0].style.color = "black";
+    paragraf[1].style.color = "black";
+    paragraf[2].style.color = "black";
+  }, 500);
+}, 1000);
+
+// ########################################################################################################
+// membuat elemen dengan DOM API
+var div3 = document.getElementsByClassName("div3")[0];
+
+// membuat elemen h1
+var judul = document.createElement("h4");
+
+// mengisi konten elemen
+judul.textContent = "ini adalah isi dari elemen h4 yand ditambah peke DOM api";
+
+// menambahkan elemen ke dalam tag body
+document.body.append(judul); // akan tampil di paling bawah
+div3.appendChild(judul); // akan tampil di dalam elemen div
+
+// ########################################################################################################
+// atau menggunakan inner html
+div3.innerHTML +=
+  "<h5>ini adalah h5 yang ditambahkan menggunakan div3.innerHTML += ''</h5>";
+
+// ########################################################################################################
+// menghapus elemen
+var removeP = document.getElementById("remove");
+removeP.remove();
+
+console.log(removeP);
