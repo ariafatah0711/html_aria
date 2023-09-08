4a30f632163ae04d1ec8604f05f4b76c5015a66e
ariafatah <ariafatah0711@gmail.com>
2023-09-08 23:18:24 +0700

    update

--- web project/04 - image - slider/script/script2.js
+++ web project/04 - image - slider/script/script2.js
@@ -1,5 +1,7 @@
 const carousel = document.querySelector(".carousel");
-firstCarouselContent = carousel.querySelectorAll(".carousel-content")[0];
+var firstCarouselContent = carousel.querySelectorAll(".carousel-content")[0];
+const carouselContents = carousel.querySelectorAll(".carousel-content");
+
 const arrowIcons = document.querySelectorAll(".wrapper i");
 
 arrowIcons.forEach((icon) => {
@@ -13,40 +15,39 @@ arrowIcons.forEach((icon) => {
   });
 });
 
-
-const carousel = document.querySelector(".carousel");
-const carouselContents = carousel.querySelectorAll(".carousel-content");
-const arrowIcons = document.querySelectorAll(".wrapper i");
-
-// Fungsi untuk mengatur jumlah elemen per baris berdasarkan lebar layar
-function setElementsPerRow() {
-  const screenWidth = window.innerWidth;
-  let elementsPerRow = 3; // Jumlah elemen per baris default
-
-  if (screenWidth <= 900) {
-    elementsPerRow = 2; // Mengubah menjadi 2 elemen per baris saat lebar layar <= 900px
-  }
-
-  if (screenWidth <= 550) {
-    elementsPerRow = 1; // Mengubah menjadi 1 elemen per baris saat lebar layar <= 550px
-  }
-
-  // Mengatur lebar elemen berdasarkan jumlah elemen per baris
-  const elementWidth = 100 / elementsPerRow;
-  carouselContents.forEach((content) => {
-    content.style.width = `${elementWidth}%`;
-  });
+function resetCarousel() {
+  carousel.scrollLeft = 0;
 }
 
-// Panggil fungsi setElementsPerRow() saat halaman dimuat dan saat window diubah ukurannya
-window.addEventListener("DOMContentLoaded", setElementsPerRow);
-window.addEventListener("resize", setElementsPerRow);
-
-// Event listener untuk tombol panah (sama seperti sebelumnya)
-arrowIcons.forEach((icon) => {
-  icon.addEventListener("click", () => {
-    let firstCarouselContentWidth = carouselContents[0].clientWidth;
-    carousel.scrollLeft +=
-      icon.id == "left" ? -firstCarouselContentWidth : firstCarouselContentWidth;
-  });
+window.addEventListener("resize", resetCarousel);
+window.addEventListener("DOMContentLoaded", () => {
+  resetCarousel();
 });
+
+// const carousel = document.querySelector(".carousel");
+// const carouselContents = carousel.querySelectorAll(".carousel-content");
+// const arrowIcons = document.querySelectorAll(".wrapper i");
+
+// // Fungsi untuk mengatur jumlah elemen per baris berdasarkan lebar layar
+// function setElementsPerRow() {
+//   const screenWidth = window.innerWidth;
+//   let elementsPerRow = 3; // Jumlah elemen per baris default
+
+//   if (screenWidth <= 900) {
+//     elementsPerRow = 2; // Mengubah menjadi 2 elemen per baris saat lebar layar <= 900px
+//   }
+
+//   if (screenWidth <= 550) {
+//     elementsPerRow = 1; // Mengubah menjadi 1 elemen per baris saat lebar layar <= 550px
+//   }
+
+//   // Mengatur lebar elemen berdasarkan jumlah elemen per baris
+//   const elementWidth = 100 / elementsPerRow;
+//   carouselContents.forEach((content) => {
+//     content.style.width = `${elementWidth}%`;
+//   });
+// }
+
+// // Panggil fungsi setElementsPerRow() saat halaman dimuat dan saat window diubah ukurannya
+// window.addEventListener("DOMContentLoaded", setElementsPerRow);
+// window.addEventListener("resize", setElementsPerRow);
