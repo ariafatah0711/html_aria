cf34aa4bfebff48bf81176208278739323e0c4c0
ariafatah <ariafatah0711@gmail.com>
2023-09-08 20:54:57 +0700

    update

---
+++ web project/04 - image - slider/script/script2.js
@@ -0,0 +1,14 @@
+const carousel = document.querySelector(".carousel");
+firstCarouselContent = carousel.querySelectorAll("carasoul-content")[0];
+const arrowIcons = document.querySelectorAll(".wrapper i");
+
+arrowIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let firstCarouselContentWidth = firstCarouselContent.clientWidth; // getting first img width & adding 14 margin value
+    // if click icon is left, reduce value from the carousel scroll left, else add to it
+    carousel.scrollLeft +=
+      icon.id == "left"
+        ? -firstCarouselContentWidth
+        : firstCarouselContentWidth;
+  });
+});
