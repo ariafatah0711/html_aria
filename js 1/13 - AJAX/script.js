1375e6fd6c7e4f65538fc7031b482f13c7815db3
ariafatah <ariafatah0711@gmail.com>
2023-09-09 19:19:52 +0700

    update

---
+++ js 1/13 - AJAX/script.js
@@ -0,0 +1,16 @@
+/* 
+// membuat object AJAX
+var xhr = new XMLHttpRequest();
+
+// menentukan fungsi handler untuk event
+xhr.onreadystatechange = function () {};
+xhr.onload = function () {};
+xhr.onerror = function () {};
+xhr.onprogress = function () {};
+
+// menentukan method dan URL
+xhr.open("GET", url, true);
+
+// mengirim request
+xhr.send();
+*/
