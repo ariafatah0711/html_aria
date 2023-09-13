ab31675567eab53696e270457d86c1a887e15359
ariafatah <ariafatah0711@gmail.com>
2023-09-13 21:15:50 +0700

    update

--- js 1/13 - AJAX/script2.js
+++ js 1/13 - AJAX/script2.js
@@ -1,3 +1,5 @@
+// ###################################################################################################
+// JQuery
 // load data ke elemen tertentu via AJAX
 $(Selector).load(URL, data, callback);
 
@@ -6,3 +8,47 @@ $.get(URL, callback);
 
 // kirim data dari server
 $.post(URL, data, callback);
+
+// ###################################################################################################
+// fetch
+fetch("http://example.com/movies/.json")
+  .then(function (response) {
+    return response.json;
+  })
+  .then(function (myJson) {
+    console.log(JSON.stringify(myJson));
+  });
+
+fetch(url, {
+  method: "POST", // GET, POST, PUT, DELETE, etc
+  mode: "cors", // no-cors, cors, *same-origin
+  chace: "no-chache", // *default, no-chache, reload, force-chace, only-if-chaced
+  credentials: "same-origin", // include, *same-origin, omit
+  headers: {
+    "Content-type": "application/json",
+    "Content-type": "application/x-www-form-urlencoded",
+  },
+  redirect: "follow", // manual, *follow, error
+  referrer: "no-referrer", // no-referrer, *client
+  body: JSON.stringify(data), // body data type must match "Content-type" header
+}).then((response) => response.json()); // parser JSON response into native javascript object
+
+// penggunaan
+fetch("https://api.example.com/data")
+  .then((response) => {
+    // response
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json; // if true response atau jika benar diresponse
+  })
+  .then((data) => {
+    console.log(data); // melakukan sesuatu dengan data yang diterima
+  })
+  .catch((error) => {
+    console.error("there was a problem with the fetch operation", error);
+  });
+
+// ###################################################################################################3
+// axios
+axios.get("https://jsonplaceholder.typicode.com/posts");
