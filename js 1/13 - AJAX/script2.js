21dffd296e667973701e59b3eac5905669d94f27
ariafatah <ariafatah0711@gmail.com>
2023-09-13 19:44:34 +0700

    update

---
+++ js 1/13 - AJAX/script2.js
@@ -0,0 +1,8 @@
+// load data ke elemen tertentu via AJAX
+$(Selector).load(URL, data, callback);
+
+// ambil data dari server
+$.get(URL, callback);
+
+// kirim data dari server
+$.post(URL, data, callback);
