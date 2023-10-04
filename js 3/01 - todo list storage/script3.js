d1184907eccb663b53de30133120214961152ed4
Aria Fatah <ariafatah0711@gmail.com>
2023-10-04 17:02:20 +0700

    update

---
+++ js 3/01 - todo list storage/script3.js
@@ -0,0 +1,7 @@
+let display = document.getElementById("display");
+
+function showData() {
+  let card = document.createElement("div");
+  let h5 = document.createElement("h5");
+  h5.innerText = data.username;
+}
